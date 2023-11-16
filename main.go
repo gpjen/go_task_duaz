bfe10fdc62a3c6c79d3aad1bf17dbb27051f0542
gpjen95 <nyonggpj9995@gmail.com>
2023-11-17 05:03:51 +0700

    add user register & login endpoint

--- main.go
+++ main.go
@@ -1,27 +1,33 @@
 package main
 
 import (
+	"log"
+	"user-product-management/app/users"
+	"user-product-management/db"
+	"user-product-management/handler"
+
 	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
 )
 
 func main() {
-
 	app := fiber.New()
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Panic("Error loading .env file")
+	}
+
+	db.InitMysqlDB()
 
 	apiV1 := app.Group("v1")
 
-	apiV1.Get("/test", func(c *fiber.Ctx) error {
-		return c.JSON(fiber.Map{
-			"message": "oke boss",
-		})
-	})
+	userRepository := users.NewUserRepository(db.DB)
+	userServices := users.NewUserService(userRepository)
+	userhandler := handler.NewUserHandler(userServices)
 
-	apiV1.Post("/test", func(c *fiber.Ctx) error {
-
-		return c.JSON(fiber.Map{
-			"message": "oke boss",
-		})
-	})
+	apiV1.Post("/user", userhandler.Register)
+	apiV1.Post("/login", userhandler.Login)
 
 	app.Listen(":3000")
 }
