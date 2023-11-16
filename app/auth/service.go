9187300ea8a26c302a9a98ed70bb0376c3563529
gpjen95 <nyonggpj9995@gmail.com>
2023-11-17 05:39:36 +0700

    add feat: generate token by user id and verivy token

--- app/auth/service.go
+++ app/auth/service.go
@@ -1,10 +1,51 @@
 package auth
 
-import "github.com/dgrijalva/jwt-go"
+import (
+	"errors"
+	"os"
+
+	"github.com/dgrijalva/jwt-go"
+)
 
 type Service interface {
 	GenerateToken(userID int) (string, error)
 	VerifyToken(token string) (*jwt.Token, error)
 }
 
-// type
+type jwtService struct {
+}
+
+func NewService() *jwtService {
+	return &jwtService{}
+}
+
+func (s *jwtService) GenerateToken(userID int) (string, error) {
+	SECRET_KEY := os.Getenv("JWT_SECRET")
+
+	claim := jwt.MapClaims{}
+	claim["user_id"] = userID
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claim)
+	signedToken, err := token.SignedString([]byte(SECRET_KEY))
+	if err != nil {
+		return signedToken, err
+	}
+
+	return signedToken, nil
+}
+
+func (s *jwtService) VerifyToken(token string) (*jwt.Token, error) {
+	SECRET_KEY := os.Getenv("JWT_SECRET")
+
+	tokenVerify, err := jwt.Parse(token, func(t *jwt.Token) (interface{}, error) {
+		if _, ok := t.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, errors.New("invalid token")
+		}
+		return []byte(SECRET_KEY), nil
+	})
+
+	if err != nil {
+		return tokenVerify, err
+	}
+
+	return tokenVerify, nil
+}
