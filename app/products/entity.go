2bf75f88d8971279f726732c2368f101b8965534
gpjen95 <nyonggpj9995@gmail.com>
2023-11-17 04:52:30 +0700

    add entity user & product

---
+++ app/products/entity.go
@@ -0,0 +1,14 @@
+package products
+
+import "time"
+
+type Product struct {
+	ID          uint   `json:"id" gorm:"primaryKey"`
+	Name        string `json:"name" gorm:"not null,type:varchar(100)"`
+	Price       int    `json:"price" gorm:"not null"`
+	Stock       int    `json:"stock" gorm:"not null"`
+	Description string `json:"description" gorm:"not null"`
+
+	CreatedAt time.Time `json:"createdAt" gorm:"default:CURRENT_TIMESTAMP"`
+	UpdatedAt time.Time `json:"updatedAt" gorm:"default:CURRENT_TIMESTAMP"`
+}
---
+++ app/users/entity.go
@@ -0,0 +1,14 @@
+package users
+
+import "time"
+
+type User struct {
+	ID           uint   `json:"id" gorm:"primaryKey"`
+	Name         string `json:"name" gorm:"not null,type:varchar(100)"`
+	Email        string `json:"email" gorm:"not null,type:varchar(100)"`
+	Address      string `json:"address" gorm:"not null,type:varchar(200)"`
+	HashPassword string `json:"-" gorm:"not null"`
+
+	CreatedAt time.Time `json:"createdAt" gorm:"default:CURRENT_TIMESTAMP"`
+	UpdatedAt time.Time `json:"updatedAt" gorm:"default:CURRENT_TIMESTAMP"`
+}
